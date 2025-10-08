ebad38bacb4044ab8efde35a3514fe7113d1534f
Dzakri Phalosa Nugroho <71388712+KanekiCraynet@users.noreply.github.com>
2025-10-08 13:37:47 +0700

    Update security.js

--- middleware/security.js
+++ middleware/security.js
@@ -123,6 +123,7 @@ const securityMiddleware = {
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdn.tailwindcss.com", "https://cdn.plyr.io"],
       scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.tailwindcss.com", "https://cdn.plyr.io", "https://cdnjs.cloudflare.com", "https://pagead2.googlesyndication.com", "https://cdn.vercel-insights.com"],
+      scriptSrcElem: ["'self'", "'unsafe-inline'", "https://cdn.tailwindcss.com", "https://cdn.plyr.io", "https://cdnjs.cloudflare.com", "https://pagead2.googlesyndication.com", "https://cdn.vercel-insights.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:", "https:", "http:"],
       mediaSrc: ["'self'", "https:", "http:"],

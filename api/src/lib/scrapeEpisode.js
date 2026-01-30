5eec90dd5a516d1063017c10c6cde26207883478
Zee <zee@kitanime.dev>
2026-01-30 22:49:00 +0000

    feat: Enhance dark mode styling with dynamic hero section gradients and fix API environment variable access.

--- api/src/lib/scrapeEpisode.js
+++ api/src/lib/scrapeEpisode.js
@@ -1,6 +1,6 @@
 import { load } from 'cheerio';
 import axios from 'axios';
-const { BASEURL } = process.env ?? 'https://otakudesu.best';
+const BASEURL = process.env.BASEURL || 'https://otakudesu.best';
 const scrapeEpisode = async (html) => {
     const $ = load(html);
     const episode = getEpisodeTitle($);
@@ -20,7 +20,7 @@ const scrapeEpisode = async (html) => {
         has_previous_episode: previous_episode ? true : false,
         previous_episode,
         stream_url: qualityList['480p'] || stream_url,
-        steramList : qualityList,
+        steramList: qualityList,
         download_urls,
     };
 };
@@ -34,56 +34,56 @@ const getStreamUrl = ($) => {
 
 const postToGetData = async (action, action2, videoData) => {
     const tasks = Object.entries(videoData).map(async ([key, value]) => {
-      if (!value) return [key, null];
-      try {
-        const url = `https://otakudesu.best/wp-admin/admin-ajax.php`;
-        const form = new URLSearchParams();
-        form.append("id", value.id);
-        form.append("i", value.i);
-        form.append("q", value.q);
-        form.append("action", action);
-  
-        let res = await axios.post(url, form.toString(), {
-          headers: { "Content-Type": "application/x-www-form-urlencoded" }
-        });
-        const form2 = new URLSearchParams();
-        form2.append("id", value.id);
-        form2.append("i", value.i);
-        form2.append("q", value.q);
-        form2.append("action", action2);
-        form2.append("nonce", res.data.data);
-  
-        res = await axios.post(url, form2.toString(), {
-          headers: { "Content-Type": "application/x-www-form-urlencoded" }
-        });
-        const $$ = load(Buffer.from(res.data.data, "base64").toString("utf8"));
-        const pdrain_url = $$("iframe").attr("src");
-  
-        const pdarin = await axios.get(pdrain_url);
-        const $$$ = load(pdarin.data);
-        const finalUrl = $$$('meta[property="og:video:secure_url"]').attr("content");
-  
-        return [key.replace("m", ""), finalUrl];
-      } catch (err) {
-        console.error(`${key} error:`, err.message);
-        return [key, null];
-      }
+        if (!value) return [key, null];
+        try {
+            const url = `https://otakudesu.best/wp-admin/admin-ajax.php`;
+            const form = new URLSearchParams();
+            form.append("id", value.id);
+            form.append("i", value.i);
+            form.append("q", value.q);
+            form.append("action", action);
+
+            let res = await axios.post(url, form.toString(), {
+                headers: { "Content-Type": "application/x-www-form-urlencoded" }
+            });
+            const form2 = new URLSearchParams();
+            form2.append("id", value.id);
+            form2.append("i", value.i);
+            form2.append("q", value.q);
+            form2.append("action", action2);
+            form2.append("nonce", res.data.data);
+
+            res = await axios.post(url, form2.toString(), {
+                headers: { "Content-Type": "application/x-www-form-urlencoded" }
+            });
+            const $$ = load(Buffer.from(res.data.data, "base64").toString("utf8"));
+            const pdrain_url = $$("iframe").attr("src");
+
+            const pdarin = await axios.get(pdrain_url);
+            const $$$ = load(pdarin.data);
+            const finalUrl = $$$('meta[property="og:video:secure_url"]').attr("content");
+
+            return [key.replace("m", ""), finalUrl];
+        } catch (err) {
+            console.error(`${key} error:`, err.message);
+            return [key, null];
+        }
     });
-  
+
     const resultsArr = await Promise.all(tasks);
     return Object.fromEntries(resultsArr.filter(Boolean));
-  };
+};
 
-const getStreamQuality = async($) => {
+const getStreamQuality = async ($) => {
     const streamLable = $('.mirrorstream');
     const results = {};
     ["m360p", "m480p", "m720p"].forEach(q => {
         const items = streamLable.find(`ul.${q} li a`);
         const last = items
-        .filter((i, el) => {
-            const text = $(el).text().toLowerCase();
-            return text.includes("drain") || text.includes("desu");
-        }).first();
+            .filter((i, el) => {
+                const text = $(el).text().toLowerCase();
+                return text.includes("drain") || text.includes("desu");
+            }).first();
         if (last.length) {
             results[q] = JSON.parse(Buffer.from(last.attr("data-content"), "base64").toString("utf8"));
         }
@@ -95,7 +95,7 @@ const getStreamQuality = async($) => {
         const regex = /action\s*:\s*"([a-z0-9]+)"/gi;
         let match;
         while ((match = regex.exec(scriptContent)) !== null) {
-          actions.push(match[1]);
+            actions.push(match[1]);
         }
     });
     const uniqueActions = [...new Set(actions)];
@@ -185,7 +185,7 @@ const getNextEpisode = ($) => {
 };
 const getAnimeData = ($) => {
     if ($('.flir a:nth-child(3)').text().trim() === '' || $('.flir a:nth-child(3)').text() === undefined) {
-        
+
         return {
             slug: $('.flir a:first').attr('href')?.replace(`${BASEURL}/anime/`, '')?.replace('/', ''),
             otakudesu_url: $('.flir a:first').attr('href'),
